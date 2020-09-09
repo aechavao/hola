985e41c2cf6ee3360c3f4430ba224ac4299c1b20
aetxabao <aitor.etxabarren@mariaanasanz.es>
2020-09-10 00:15:20 +0200

    Ni aet

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace hola
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //@aetxabao
+            Console.WriteLine("Ni aetxabao naiz!");
         }
     }
 }
